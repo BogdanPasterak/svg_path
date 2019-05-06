2b3825a66debcbf5a2ea97983b16f89d075d9c07
bogdanpasterak <bogdanpasterak@gmail.com>
2019-05-06 16:03:32 +0100

    Move cursor

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
 const myData = {
-    x: Number,
-    y: Number
+    x: 0,
+    y: 0,
+
+    init: () => {
+        //console.log("Start");
+        x = 0;
+        y = 0;
+    }
 };
 const view = {
-    board: Element,
-    cursor: Element,
 
-
-    init: function() {
+    init: function () {
         board = document.querySelector('.board');
         cursor = document.querySelector('.cursor');
+        textarea = document.querySelector('textarea');
     },
-    rePaint: function() {
-        //console.log(board);
+    rePaint: function () {
+        //console.log(cursor);
         cursor.setAttribute('cx', myData.x);
+        cursor.setAttribute('cy', myData.y);
     }
 };
 
+function keyUpEvent(event) {
+    const key = event.key;
+    if (key === 'Control') { return; }
+
+    //console.log(document.activeElement);
+    if (document.activeElement === document.querySelector('textarea')) {
+        let pos = view.textarea.selectionStart;
+        console.log(pos, view.textarea.value);
+    } else {
+        let change = true;
+        if (key == 'ArrowRight') {
+            if (event.ctrlKey && myData.x <= 95)
+                myData.x += 5;
+            else if (myData.x < 100)
+                myData.x++;
+        } else if (key == 'ArrowLeft') {
+            if (event.ctrlKey && myData.x >= 5)
+                myData.x -= 5;
+            else if (myData.x > 0)
+                myData.x--;
+        } else if (key == 'ArrowUp') {
+            if (event.ctrlKey && myData.y >= 3)
+                myData.y -= 3;
+            else if (myData.y > 0)
+                myData.y--;
+        } else if (key == 'ArrowDown') {
+            if (event.ctrlKey && myData.y <= 17)
+                myData.y += 3;
+            else if (myData.y < 20)
+                myData.y++;
+        } else {
+            console.log(key);
+            change = false;
+        }
+
+        if (change) {
+            view.rePaint();
+        }
+    }
+    view.rePaint();
+}
+
 window.onload = function (e) {
-    console.log("Start");
-    myData.x = 1;
-    myData.y = 0;
+    //myData.init();
     view.init();
     view.rePaint();
-    document.addEventListener('keyup', (event) => {
-        const key = event.key;
-        let ta = document.querySelector('textarea');
-        let pos = ta.selectionStart;
-        myData.x++;
-        view.rePaint();
-        console.log(pos, ta.value );
-    }, false);
+    document.addEventListener('keyup', keyUpEvent, false);
 }
 
